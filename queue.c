ef787a9f36e85c6af8527e77f0279c3e03add175
Muliro1 <mulirokhaemba@gmail.com>
2023-06-21 01:06:15 +0300

    bugfixes

--- queue.c
+++ queue.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * f_queue - prints the top
+ * queue - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_queue(stack_t **head, unsigned int counter)
+void queue(stack_t **head, unsigned int counter)
 {
 	(void)head;
 	(void)counter;
